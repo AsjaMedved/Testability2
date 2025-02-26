2fc0740a05fa41007f4c4978cdbde1d61bb589d9
Asja <anastasjamedved@gmail.com>
2025-02-26 23:07:56 +0300

    initial commit1

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(double weight, double height) {
-        double bmi = weight / (height * height);  // расчет ИМТ как double
-        return (int) bmi;
+        double coefficient = weight / (height * height);  // расчет ИМТ как double
+        return (int) coefficient;
     }
 }
